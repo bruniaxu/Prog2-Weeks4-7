9e2e04e92e59e2600b82cf7fd2dab2035035b87a
tangmanp <tangmanp@OA-S141-15>
2026-05-26 17:45:49 -0400

    made spawner and runner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+//WEEK FOUR STUFF:
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject runnerPrefab;
+    public Vector3 spawnPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            //Spawn a runner!
+            Instantiate(runnerPrefab);
+
+            //Spawn a runner that is a child of this object
+            Instantiate(runnerPrefab, transform);
+
+            //spawn a runner at a specific position with no rotation:
+            Instantiate(runnerPrefab, spawnPosition, Quaternion.identity);
+
+            //POSITION OF ZERO
+            //Vector3 zeroVector = Vector3.zero;
+
+            //ROTATION OF ZERO
+            //Quaternion zeroQuaternion = Quaternion.identity;
+        }
+    }
+}
