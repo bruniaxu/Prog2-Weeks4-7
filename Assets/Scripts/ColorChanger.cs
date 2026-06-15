6eb30ba7ffe5480172d6934aa68138bfe5724a1b
Bruni Tang <bruniaxu@gmail.com>
2026-06-15 01:10:09 -0400

    color changer

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -6,25 +6,25 @@ using UnityEngine.InputSystem;
 public class ColorChanger : MonoBehaviour
 {
     public SpriteRenderer colorRenderer;
-    public Color color;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        colorRenderer.color = color;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = Mouse.current.position.ReadValue();
-        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        worldMousePosition.z = 0f;
-
-        bool isMouseClicked = Mouse.current.leftButton.wasReleasedThisFrame;
-        bool isMouseOverSprite = colorRenderer.bounds.Contains(worldMousePosition);
+   
 
         
 
+    }
+
+    public void colorChanger()
+    {
+        colorRenderer.color = new Color(Random.value, Random.value, Random.value);
     }
 }
