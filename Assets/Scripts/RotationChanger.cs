0800f468526710c7d00ac229e7f977da5f2357d2
Bruni Tang <bruniaxu@gmail.com>
2026-06-15 02:19:41 -0400

    rotation changer

---
+++ Assets/Scripts/RotationChanger.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RotationChanger : MonoBehaviour
+{
+    public SpriteRenderer RotateRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AngleRotation(float sliderValue)
+    {
+        Vector3 currentRotation = RotateRenderer.transform.eulerAngles;
+        currentRotation.z = sliderValue;
+        RotateRenderer.transform.eulerAngles = currentRotation;
+    }
+}
